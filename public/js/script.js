e1dd2cdd258e4d1d15f023a25bc25fc9b9a0a334
Himani <himani.singla@niit.com>
2018-04-13 11:31:15 +0530

    Initial Boilerplate code

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+function getMovies() {
+
+}
+
+function getFavourites() {
+
+}
+
+function addFavourite() {
+
+}
+
+module.exports = {
+	getMovies,
+	getFavourites,
+	addFavourite
+};
+
+// You will get error - Uncaught ReferenceError: module is not defined
+// while running this script on browser which you shall ignore
+// as this is required for testing purposes and shall not hinder
+// it's normal execution
+
+
